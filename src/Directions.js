0972a93aea6e8f8147680018c0691a72ae8ba641
nhatnghiho <38119460+nhatnghiho@users.noreply.github.com>
2022-01-25 00:34:22 -0500

    second commit

--- src/Directions.js
+++ src/Directions.js
@@ -1,6 +1,116 @@
-import React, { useContext } from "react";
-var MapBoxDirections = require("@mapbox/mapbox-gl-directions");
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css";
+import React, { useRef, useEffect, useState } from "react";
+import polyline from "@mapbox/polyline";
+
+var turf = require("@turf/turf");
 
 function Directions(props) {
-  //   const context = useContext(MapContext);
+  const map = props.map;
+
+  const [temp, setTemp] = useState(0);
+  const [temp2, setTemp2] = useState(0);
+
+  let bbox = [0, 0, 0, 0];
+  let polygon = turf.bboxPolygon(bbox);
+  let point = turf.point([0, 0]);
+
+  const directions = useRef(null);
+
+  useEffect(() => {
+    if (directions.current || !map.current) return;
+
+    directions.current = new MapboxDirections({
+      //   container: mapContainerRef.current,
+      accessToken: process.env.REACT_APP_MAPBOX_TOKEN,
+      profile: "mapbox/driving",
+      alternatives: false,
+      geometries: "geojson",
+      interactive: false,
+    });
+    map.current.addControl(directions.current, "top-right");
+  });
+
+  useEffect(() => {
+    directions.current.on("load", (e) => {
+      setTemp(0);
+      directions.current.removeRoutes();
+    });
+  });
+
+  useEffect(() => {
+    directions.current.on("clear", () => {
+      map.current.setLayoutProperty("bbox", "visibility", "none");
+    });
+  });
+
+  useEffect(() => {
+    map.current.on("styledata", () => {
+      if (temp2 !== 0) return;
+
+      map.current.addSource("bbox", {
+        type: "geojson",
+        data: {
+          type: "Feature",
+        },
+      });
+      map.current.addLayer({
+        id: "bbox",
+        source: "bbox",
+        type: "fill",
+        paint: {
+          "fill-color": "#FFC300",
+          "fill-opacity": 0.5,
+          "fill-outline-color": "#FFC300",
+        },
+      });
+      setTemp2(1);
+    });
+  });
+
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    directions.current.on("route", (e) => {
+      console.log(map.current.isStyleLoaded());
+      if (temp !== 0 || temp2 === 0) return;
+
+      if (counter > 6) {
+        return;
+      }
+      setCounter(counter + 1);
+      for (const route of e.route) {
+        console.log("all: " + JSON.stringify(e));
+        console.log(route);
+        // setTemp(route);
+
+        console.log(
+          "end: " + JSON.stringify(directions.current.getDestination())
+        );
+
+        console.log(directions.current.getDestination().geometry.coordinates);
+
+        polygon = turf.buffer(directions.current.getDestination().geometry, 5);
+        console.log(polygon);
+        point = turf.randomPoint(1, { bbox: turf.bbox(polygon) });
+        console.log(point);
+
+        directions.current.setWaypoint(
+          0,
+          point["features"][0].geometry.coordinates
+        );
+
+        map.current.getSource("bbox").setData(polygon);
+        map.current.setLayoutProperty("bbox", "visibility", "visible");
+      }
+      setTemp(1);
+    });
+  });
+
+  //   useEffect(() => {
+  //     directions.setWayPoint(0);
+  //   });
+  return <div></div>;
 }
+
+export default Directions;
--- src/Map.js
+++ src/Map.js
@@ -1,39 +1,53 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
+import Directions from "./Directions";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "./Map.css";
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 function Map() {
-  const MapContext = React.createContext;
   const mapContainerRef = useRef(null);
+  const map = useRef(null);
+
+  const [directions, setDirections] = useState();
 
   const [lng, setLng] = useState(-84.3902644);
   const [lat, setLat] = useState(33.7489924);
   const [zoom, setZoom] = useState(10);
 
   useEffect(() => {
-    const map = new mapboxgl.Map({
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
       container: mapContainerRef.current,
       style: "mapbox://styles/nhatnghiho/ckyq8sj140wvd15qb7qbom1u5",
       center: [lng, lat],
       zoom: zoom,
     });
 
-    map.addControl(new mapboxgl.NavigationControl(), "top-right");
+    setDirections(
+      <Directions
+        // ref={map}
+        map={map}
+        mapContainerRef={mapContainerRef}
+      />
+    );
+  });
 
-    map.on("move", () => {
-      setLng(map.getCenter().lng.toFixed(4));
-      setLat(map.getCenter().lat.toFixed(4));
-      setZoom(map.getZoom().toFixed(2));
+  useEffect(() => {
+    if (!map.current) return;
+    map.current.on("move", () => {
+      setLng(map.current.getCenter().lng.toFixed(4));
+      setLat(map.current.getCenter().lat.toFixed(4));
+      setZoom(map.current.getZoom().toFixed(2));
     });
-    return () => map.remove();
-  }, []);
+  });
 
   return (
     <div>
-      <div className="map-container" ref={mapContainerRef} />
+      <div id="main" className="map-container" ref={mapContainerRef}>
+        {directions}
+      </div>
     </div>
   );
 }
